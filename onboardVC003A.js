c92b407a50059a504aca7c7c288abb4870ee2092
visionarycrest-vc001 <PERCY@WE-HEARTTRANSPORT.COM>
2025-08-17 11:42:47 -0700

    onboardVC003A.js

---
+++ onboardVC003A.js
@@ -0,0 +1,27 @@
+// onboardVC003A.js
+const fs = require('fs');
+const dashboardHooks = require('./dashboardHooks');
+const scrollInscription = require('./scrollInscription');
+
+function activateCohort(cohortName) {
+  console.log(`Activating ${cohortName}...`);
+
+  // Register stewards
+  const stewards = [
+    'Steward001', 'Steward002', 'Steward003', // Add all 12
+  ];
+
+  stewards.forEach(steward => {
+    console.log(`Registering ${steward}...`);
+    // Simulate oath capture
+    console.log(`${steward} oath: "I uphold the glyphs of resilience: ⧉⚘⟒⟊."`);
+  });
+
+  // Pair scrolls
+  scrollInscription.pairScrolls('VC003');
+
+  // Sync dashboard
+  dashboardHooks.activateMetrics('VC003');
+}
+
+module.exports = { activateCohort };
