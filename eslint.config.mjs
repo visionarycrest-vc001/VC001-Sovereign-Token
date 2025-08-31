34c0eb8f4c30d5f7cf5b660d4c08c6183ba2ef04
visionarycrest-vc001 <PERCY@WE-HEARTTRANSPORT.COM>
2025-08-31 15:22:12 -0700

    Refactor ESLint config for TypeScript support
    Signed-off-by: visionarycrest-vc001 <PERCY@WE-HEARTTRANSPORT.COM>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,41 @@
 import js from "@eslint/js";
 import react from "eslint-plugin-react";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 
 export default [
   js.configs.recommended,
-  tseslint.configs.recommended, // Enable recommended TypeScript rules
   {
-    files: ["**/*.{js,ts,jsx,tsx}"],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
-      globals: {
-        ...globals.browser,
-      },
+      globals: { ...globals.browser },
     },
-    plugins: {
-      react,
-      "@typescript-eslint": tseslint.plugin,
-    },
+    plugins: { react },
     rules: {
-      // Add custom rules below
       semi: ["error", "always"],
       quotes: ["error", "double"],
       "react/prop-types": "off",
-      "@typescript-eslint/no-unused-vars": ["warn"],
-      "@typescript-eslint/explicit-function-return-type": "off",
     },
     settings: {
-      react: {
-        version: "detect",
-      },
+      react: { version: "detect" },
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { ...globals.browser },
+    },
+    plugins: { "@typescript-eslint": tseslint },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
     },
   },
 ];
