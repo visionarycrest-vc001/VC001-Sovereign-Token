423172a0b1883334e99cdca2bc48151bd5b4c6de
visionarycrest-vc001 <PERCY@WE-HEARTTRANSPORT.COM>
2025-08-30 22:06:55 -0700

    Add ESLint configuration for JavaScript files
    Signed-off-by: visionarycrest-vc001 <PERCY@WE-HEARTTRANSPORT.COM>

---
+++ .github/workflows/eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module"
+    },
+    rules: {
+      semi: ["error", "always"]
+    }
+  }
+]
