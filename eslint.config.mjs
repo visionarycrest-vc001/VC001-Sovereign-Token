bbe17d51d7f0a66ddca075d11a9fb55017ca6256
visionarycrest-vc001 <PERCY@WE-HEARTTRANSPORT.COM>
2025-08-31 15:19:54 -0700

    Add TypeScript support to ESLint configuration
    Signed-off-by: visionarycrest-vc001 <PERCY@WE-HEARTTRANSPORT.COM>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import js from "@eslint/js";
 import react from "eslint-plugin-react";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
+  tseslint.configs.recommended, // Enable recommended TypeScript rules
   {
-    files: ["**/*.js"],
+    files: ["**/*.{js,ts,jsx,tsx}"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -15,9 +17,15 @@ export default [
     },
     plugins: {
       react,
+      "@typescript-eslint": tseslint.plugin,
     },
     rules: {
-      // Add custom rules here
+      // Add custom rules below
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      "react/prop-types": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/explicit-function-return-type": "off",
     },
     settings: {
       react: {
